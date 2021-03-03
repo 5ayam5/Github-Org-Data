901f4b61bb1010a810b504e3c6961027b989ef66
5ayam5 <sayam.sethi2@gmail.com>
2021-03-03 23:23:39 +0530

    done

--- src/index.js
+++ src/index.js
@@ -2,104 +2,91 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-var org, repoC, commC;
-
-class Org extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputValue: ''
-    };
-  }
-
-  render() {
-    return (
-      <input type="text" value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)}/>
-    );
-  }
-
-  updateInputValue(evt) {
-    this.setState({
-      inputValue: evt.target.value
-    });
-    org = evt.target.value;
-  }
-}
-
-class RepoC extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputValue: '1'
-    };
-  }
-
-  render() {
-    return (
-      <input type="number" min="1" value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)}/>
-    );
-  }
-
-  updateInputValue(evt) {
-    this.setState({
-      inputValue: evt.target.value
-    });
-    repoC = evt.target.value;
-  }
-}
-
-class CommC extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputValue: '1'
-    };
-  }
-
-  render() {
-    return (
-      <input type="number" min="1" value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)}/>
-    );
-  }
-
-  updateInputValue(evt) {
-    this.setState({
-      inputValue: evt.target.value
-    });
-    commC = evt.target.value;
-  }
-}
-
-class Submit extends React.Component {
-  render() {
-    return (
-      <button>Query</button>
-    )
-  }
-}
-
-class Query extends React.Component {
-  render() {
-    return (
-      <div className="query">
-        <form>
-          <Org />
-          <RepoC />
-          <CommC />
-          <Submit />
-        </form>
-      </div>
-    )
-  }
-}
-
 class Root extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      org: '',
+      commC: '1',
+      repoC: '1',
+      list: []
+    }
+  }
+
   render() {
     return (
       <div className="document">
-        <Query />
+        <div className="query" onSubmit={this.handleSubmit.bind(this)}>
+          <form>
+            <input type="text" value={this.state.org} onChange={this.updateOrg.bind(this)} />
+            <input type="number" min="1" max="100" value={this.state.repoC} onChange={this.updateRepoC.bind(this)} />
+            <input type="number" min="1" max="100" value={this.state.commC} onChange={this.updateCommC.bind(this)} />
+            <button>Query</button>
+          </form>
+        </div>
+        <div className="result">
+          <ol>
+            {this.state.list.map((repo, index) => (
+              <li key={index}>
+                <a href={repo.url}>{repo.name}</a> ({repo.forks} forks)
+                <ul>
+                  {repo.contributors && (repo.contributors).map((contributor, idx) => (
+                    <li key={idx}><a href={contributor.url}>{contributor.name}</a> ({contributor.contributions} contributions)</li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ol>
+        </div>
       </div>
     );
+  }
+
+  updateOrg(event) {
+    this.setState({ org: event.target.value })
+  }
+  updateRepoC(event) {
+    this.setState({ repoC: event.target.value })
+  }
+  updateCommC(event) {
+    this.setState({ commC: event.target.value })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    fetch('https://api.github.com/orgs/' + this.state.org + '/repos?per_page=100').then(response => response.json()).then(response => this.displayResults(response));
+  }
+
+  displayResults(arr) {
+    try {
+      arr.sort(function (a, b) {
+        return b.forks_count - a.forks_count;
+      });
+    } catch (err) {
+      alert("Error in retrieving data, please try again");
+      return;
+    }
+    arr = arr.slice(0, this.state.repoC);
+    let ret = [];
+    arr.forEach(repo => {
+      let temp = {
+        name: repo.name,
+        url: repo.svn_url,
+        forks: repo.forks,
+        contributors: []
+      };
+      try {
+        fetch(repo.contributors_url + '?per_page=' + this.state.commC).then(response => response.json()).then(response => response.forEach(contributor => {
+          temp.contributors.push({
+            name: contributor.login,
+            url: contributor.html_url,
+            contributions: contributor.contributions
+          })
+        })).then(response => ret.push(temp)).then(response => this.setState({ list: ret }));
+      } catch (err) {
+        this.setState({ list: ret });
+      }
+    });
   }
 }
 
